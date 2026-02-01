d232ee15d672cb4c390ec519c4a64a8933301ac5
Abdellatif LAMRABTI <abdellatif@darbtech.net>
2026-02-01 21:13:35 +0100

    add repo

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Chems Custom",
+    "summary": "",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "author": "Lamrabti Abdellatif",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "report/order_document.xml",
+    ],
+    "installable": True,
+    "application": True
+}
